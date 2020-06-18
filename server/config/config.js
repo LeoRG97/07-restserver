4ceb6daa893996a9d1575d9b3bdf96528a882238
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2020-06-18 18:06:52 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://darth_vinci:dbcFtcGx512P6hy7@cluster0-5ltnm.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
